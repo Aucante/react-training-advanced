aa71d08f2141b2ad375fa563c85c705a7355241e
Aucante <sebastien.aucante@gmail.com>
2022-10-15 12:21:29 +0200

    never change muted state

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
 import './App.css';
-import {useState, useEffect, useRef} from 'react'
+import {useState} from 'react'
 import Video from './video.mp4'
 
 function App() {
 
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(1);
 
-  const ref = useRef([]);
-
-  useEffect(() => {
-    window.addEventListener('resize', actionResize);
-
-    function actionResize(){
-      console.log("Resized");
-    }
-
-    return () => {
-      window.removeEventListener('resize', actionResize)
-    }
-  }, [])
-  
 
   const toggleFunc = () => {
-    setToggle(!toggle);
+    setToggle(toggle + 1);
   }
 
-  const addToRef = el => {
-    if(el && !ref.current.includes(el)){
-      ref.current.push(el);
-    }
-  }
 
   return (
     <div className="App">
-      <video ref={addToRef} width="750" height="500" autoPlay controls muted>
-        <source src={Video} type='video/mp4'/>
-      </video>
-      <video ref={addToRef} width="750" height="500" autoPlay controls muted>
-        <source src={Video} type='video/mp4'/>
-      </video>
-      <video ref={addToRef} width="750" height="500" autoPlay controls muted>
-        <source src={Video} type='video/mp4'/>
-      </video>
+      <h1>{toggle}</h1>
       
       <button onClick={toggleFunc}>Toggle</button>
     </div>
