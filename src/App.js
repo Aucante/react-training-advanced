518a37a7a48ae12e7f45e1d24d22830102fa0b9c
Aucante <sebastien.aucante@gmail.com>
2023-01-13 21:58:27 +0100

    test 5

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
     <div className="App">
       <Content />
       <button onClick={toggleFunc}>Toggle</button>
+      <button onClick={toggleFunc}>Toggle</button>
     </div>
   );
 }
